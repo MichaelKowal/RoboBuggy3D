399f39209bca4af9f41c60a7c242ad3d0bea5875
Michael Kowal <Mchlkwl@gmail.com>
2018-05-25 21:12:35 -0700

    Mazes can be created
    Still lots of optimization to do but random mazes can be made and traversed.

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -12,26 +12,6 @@ public class Cube : MonoBehaviour {
 	{
 		gridPoint = gp;
 		transform.SetParent(GameObject.Find("Board").transform);
-		getNeighbours();
 	}
 
-    //find all adjacent cubes
-	public void getNeighbours()
-	{
-		for (int i = gridPoint.x - 1; i < gridPoint.x + 2; i++)
-		{
-			for (int j= gridPoint.z - 1; j < gridPoint.z + 2; j++)
-            {
-				if (BoardManager.Instance.Cubes.ContainsKey(new GridPoint(i, j)))
-				{
-					neighbours.Add(BoardManager.Instance.Cubes[new GridPoint(i, j)]);
-				}
-            }
-		}
-        //add yourself to all your neighbours' lists
-		foreach(Cube neighbour in neighbours)
-		{
-			neighbour.neighbours.Add(this);
-		}
-	}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour 
 {
-	bool isJumping;
+	[SerializeField]
+	int speed = 10;
+
 	// Update is called once per frame
 	void Update ()
 	{
@@ -12,22 +14,22 @@ public class Player : MonoBehaviour
         //move forward
 		if(Input.GetKey("w") && !Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1))
 		{
-			transform.Translate(Vector3.forward * Time.smoothDeltaTime * GameManager.Instance.cameraSpeed);
+			transform.Translate(Vector3.forward * Time.smoothDeltaTime * speed);
 		}
         //move left
 		if (Input.GetKey("a") && !Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out hit, 1))
         {
-			transform.Translate(Vector3.left * Time.smoothDeltaTime * GameManager.Instance.cameraSpeed);
+			transform.Translate(Vector3.left * Time.smoothDeltaTime * speed);
         }
         //move back
 		if (Input.GetKey("s") && !Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out hit, 1))
         {
-			transform.Translate(Vector3.back * Time.smoothDeltaTime * GameManager.Instance.cameraSpeed);
+			transform.Translate(Vector3.back * Time.smoothDeltaTime * speed);
         }
         //move right
 		if (Input.GetKey("d") && !Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit, 1))
         {
-			transform.Translate(Vector3.right * Time.smoothDeltaTime * GameManager.Instance.cameraSpeed);
+			transform.Translate(Vector3.right * Time.smoothDeltaTime * speed);
         }
         //rotate view with mouse
 		if(Input.GetAxis("Horizontal") != 0)
